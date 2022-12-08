5aa5342c47f498cb635cd44a9f8a929b0b58097e
ekgp209 <k2057217@naver.com>
2022-12-09 01:43:15 +0900

    React 적용해보기
    돌고돌아 이번엔 react로 시도..! 블로그보고 따라 만드는 중

---
+++ simple-react-board/src/views/Article.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Article() {
+    return <div>게시글 상세</div>;
+}
+
+export default Article;
---
+++ simple-react-board/src/views/Post.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Post() {
+    return <div>글쓰기</div>;
+}
+
+export default Post;
